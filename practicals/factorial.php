d73ce2525cab52eea00c247b0ed062f06faa1ab7
girishnasre246 <girishnasre246@gmail.com>
2023-11-01 21:57:56 +0530

    Make factorial  code readable and maintanable

--- practicals/factorial.php
+++ practicals/factorial.php
@@ -1,7 +1,10 @@
 <?php
+// Function to calculate the factorial
 function factorial($n) {
-    if ($n === 0) {
-        return 1;
+    if ($n < 0) {
+        return "Factorial is not defined for negative numbers.";
+    } elseif ($n == 0) {
+        return 1; // Factorial of 0 is 1
     } else {
         $result = 1;
         for ($i = 1; $i <= $n; $i++) {
@@ -11,9 +14,14 @@ function factorial($n) {
     }
 }
 
-// Replace the number below with the one for which you want to calculate the factorial.
-$number = 5;
-$fact = factorial($number);
-echo "Factorial of $number is $fact";
-?>
+// Number for which you want to calculate the factorial
+$number = 5; // Change this to the desired number
 
+if ($number >= 0) {
+        $fact = factorial($number);
+        // Factorial of a number
+    echo "Factorial of $number is $fact";
+} else {
+    echo "Factorial is not defined for negative numbers.";
+}
+?>
