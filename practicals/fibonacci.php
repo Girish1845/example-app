93e26655a24f020067bc3888f10f911725c0cdb3
girishnasre246 <girishnasre246@gmail.com>
2023-11-01 21:54:10 +0530

    Make fibonacci  code readable and maintanable

--- practicals/fibonacci.php
+++ practicals/fibonacci.php
@@ -1,9 +1,7 @@
 <?php
 // Function to generate the Fibonacci sequence up to a given number of terms
 function generateFibonacci($n) {
-    $fibonacci = array();
-    $fibonacci[0] = 0;
-    $fibonacci[1] = 1;
+    $fibonacci = [0,1];
 
     for ($i = 2; $i < $n; $i++) {
         $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
@@ -12,12 +10,13 @@ function generateFibonacci($n) {
     return $fibonacci;
 }
 
-// Number of terms you want in the Fibonacci sequence
-$terms = 10; // Change this to the desired number of terms
+// Number of terms in the Fibonacci sequence
+$noterms = 10; // Change this to the desired number of terms
 
-if ($terms >= 1) {
-    $result = generateFibonacci($terms);
-    echo "Fibonacci sequence for the first $terms terms: ";
+if ($noterms >= 1) {
+        $result = generateFibonacci($noterms);
+        // Display fibonacci sequence
+    echo "Fibonacci sequence for the first $noterms terms: ";
     echo implode(", ", $result);
 } else {
     echo "Please specify a valid number of terms (>= 1) for the Fibonacci sequence.";
