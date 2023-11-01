d6a35035d450b00d8a0f3859e37912fd4702b3ff
girishnasre246 <girishnasre246@gmail.com>
2023-11-01 21:49:20 +0530

    Make number swapping  code readable and maintanable

--- practicals/number_swapping.php
+++ practicals/number_swapping.php
@@ -4,7 +4,8 @@ $number1 = 5;
 $number2 = 10;
 
 echo "Before swapping: ";
-echo "Number 1 = " . $number1 . ", Number 2 = " . $number2 . "<br>";
+//Display initial values
+echo "Number 1 = " . $number1 . ", Number 2 = " . $number2 . "\n";
 
 // Swap the numbers using a temporary variable
 $temp = $number1;
@@ -12,5 +13,6 @@ $number1 = $number2;
 $number2 = $temp;
 
 echo "After swapping: ";
+//Display initial values
 echo "Number 1 = " . $number1 . ", Number 2 = " . $number2;
 ?>
